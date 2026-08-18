c22b26b620a74f77bcc050f1bf7fa3cc5e76c8ec
thomasdzuba-dev <thomas.dzuba@gmail.com>
2026-08-18 12:39:18 -0400

    Update main.tf
    Replace deprecated Bitnami AMI with Ubuntu OS

--- main.tf
+++ main.tf
@@ -1,22 +1,31 @@
-data "aws_ami" "app_ami" {
+data "aws_ami" "ubuntu" {
   most_recent = true
+  owners      = ["099720109477"] # Canonical's official AWS account ID
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["ubuntu/images/hvm-ssd*/ubuntu-jammy-22.04-amd64-server-*"]
   }
 
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["979382823631"] # Bitnami
 }
 
+# 2. Deploy the instance and install Tomcat on boot
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.app_ami.id
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.nano"
+
+  # This bash script runs once during the very first boot to install Tomcat
+  user_data = <<-EOF
+              #!/bin/bash
+              apt-get update -y
+              apt-get install -y tomcat9
+              systemctl enable tomcat9
+              systemctl start tomcat9
+              EOF
 
   tags = {
     Name = "HelloWorld"
